4b304cfcddc494ba53c68113900c930267d49502
Zach <zach@Zachs-MacBook-Pro.local>
2019-10-26 17:22:00 -0700

    modify image_loader.py

---
+++ image_uploader.py
@@ -0,0 +1,26 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','trainer_project.settings')
+
+import django
+django.setup()
+
+from trainer_app.models import *
+
+def load_media():
+	for file in os.listdir():
+		exercise_name = file.split('.')[0]
+		try:
+			exercise = Exercise.objects.get(name=exercise_name)
+			exercise.image = 'exercise_demos/{0}'.format(file)
+			exercise.save()
+			print('success: ' + exercise_name)
+		except Exception as e:
+			print(exercise_name)
+			print(e)
+
+def read_dir()
+	print(os.getcwd())
+
+if __name__ == "__main__":
+	#load_media()
+	read_dir()
