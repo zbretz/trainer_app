212f1a13c2bc8f5c92586c8bfe12f43edc4defbc
Zach <zach@Zachs-MacBook-Pro.local>
2019-10-29 12:41:04 -0700

    testing video on workout page

--- static/js/trainer-ajax.js
+++ static/js/trainer-ajax.js
@@ -13,6 +13,12 @@ for (i = 0; i < acc.length; i++) {
     } else {
       panel.style.display = "block";
     }
+
+    console.log(panel.childNodes)
+
+    var video = panel.childNodes[1]
+    video.play()
+
   });
 }
 
