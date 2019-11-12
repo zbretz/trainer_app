607fe1968a78c2a4b0ea0d20373a5789a3ffe263
Zach <zach@Zachs-MacBook-Pro.local>
2019-11-12 11:13:47 -0800

    merge js files

--- static/js/trainer-ajax.js
+++ static/js/trainer-ajax.js
@@ -1,7 +1,20 @@
 
 $(document).ready(function() {
 
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
 
 
 var allimages= document.getElementsByTagName('img');
@@ -11,14 +24,12 @@ var allimages= document.getElementsByTagName('img');
         }
     }
 
-
 /*
 function pageLoad() {
     var nodeList, index;
     nodeList = document.body.getElementsByTagName('img');
     index = 0;
     backgroundLoader();
-
     function backgroundLoader() {
         var img, src;
         if (index >= nodeList.length) {
@@ -35,7 +46,6 @@ function pageLoad() {
         window.setTimeout(backgroundLoader, 200);
     }
 }    
-
 pageLoad()
 */
 
@@ -43,14 +53,11 @@ pageLoad()
 	$('#complete').click(function() {
   		$(this).hide()
   		$('#next').show()
-
   		$.get('/trainer_app/log_workout_time/', {'category_id': 'test'},
 		function(data) {
-
 			if (data === 'True'){
 				alert('hjghg')
 			}
-
 		})
 	*/
 
@@ -61,13 +68,10 @@ pageLoad()
 	$('#like_btn').click(function() {
 	var category_id_var;
 	category_id_var = $(this).attr('data-categoryid');
-
 	$.get('/rango/like_category/', {'category_id': category_id_var},
 		function(data) {
 			$('#like_count').html(data);
     		$('#like_btn').hide();
 		})
 	});
-
-*/	
-
+*/
