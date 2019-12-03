2058f8a83e1d03544ace5f123b174b658f7be2d9
Zach <zach@Zachs-MacBook-Pro.local>
2019-12-02 18:53:48 -0800

    refactor wokrout_loader to enable adding user from command line

--- workout_loader.py
+++ workout_loader.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import sys
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','trainer_project.dev_settings')
 
 import django
@@ -45,32 +46,40 @@ def read_csv_old():
 					unit.exercises.add(exercise[0])
 			workout.units.add(unit)
 
-def create_user():
-	group1 = Group.objects.get(group_name='group1')
-	group2 = Group.objects.get(group_name='group2')
+def create_user(user, group):
+	#group1 = Group.objects.get(group_name='group1')
+	#group2 = Group.objects.get(group_name='group2')
 
-	workout1 = Workout.objects.get(group=group1, workout_number=1)
-	workout2 = Workout.objects.get(group=group2, workout_number=1)
+	#workout1 = Workout.objects.get(group=group1, workout_number=1)
+	#workout2 = Workout.objects.get(group=group2, workout_number=1)
 
-	u1 = User(username='Zach', password = 'pass')
-	u1.set_password(u1.password)
-	u1.save()
-	u1 = UserProfile(user = u1, name='Zach', group=group1, current_workout=workout1)
-	u1.save()
-
-	u2 = User(username='Kate', password = 'pass')
-	u2.set_password(u2.password)
-	u2.save()
-
-	u2 = UserProfile(user=u2, name='Kate', group=group2, current_workout=workout2)
-	u2.save()
-
-
-	#u1 = User(name='Zach', group=group1, current_workout=workout1)
+	#u1 = User(username='Zach', password = 'pass')
+	#u1.set_password(u1.password)
 	#u1.save()
-	#u2 = User(name='Kate', group=group2, current_workout=workout2)
+	#u1 = UserProfile(user = u1, name='Zach', group=group1, current_workout=workout1)
+	#u1.save()
+
+	#u2 = User(username='Kate', password = 'pass')
+	#u2.set_password(u2.password)
 	#u2.save()
 
+	#u2 = UserProfile(user=u2, name='Kate', group=group2, current_workout=workout2)
+	#u2.save()
+
+
+	group = Group.objects.get(group_name=group)
+	workout = Workout.objects.get(group=group, workout_number=1)
+
+	user = User(username=user, password = 'hustle!')
+	user.set_password(user.password)
+	user.save()
+	user_profile = UserProfile(user = user, name=user.username, group=group, current_workout=workout)
+	user_profile.save()
+
+
 if __name__ == "__main__":
-	read_csv_new()
-	create_user()
+	if 'create_user' in sys.argv:
+		create_user(user=sys.argv[2], group=sys.argv[3])
+	else:
+		read_csv_new()
+	#create_user()
